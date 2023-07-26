fe3978ce214fd3de6a1f11c04f3d35e570f3ae35
Andrey Sobolev <chorzoww@gmail.com>
2023-07-27 01:44:13 +0800

    added scaling curve plotting functions

--- akagi/visuals.py
+++ akagi/visuals.py
@@ -59,6 +59,114 @@ def plot_cooler(clr: cooler.Cooler,
 
     return ax
 
+def scaling_curve(clr: cooler.Cooler,
+        smooth: Optional[bool] = False,
+        aggregate_smoothed: Optional[bool] = False,
+        nproc: Optional[int] = 4,
+        ax: Optional[plt.Axes] = None,
+        **kwargs) -> plt.Axes:
+    """
+    Plots a scaling curve in log-log scale with its derivative in semi-log scale.
+    Data used for plotting can be accessed via attributes of the function:
+    scaling_curve.cvd = contact-versus-distance dataframe;
+    scaling_curve.der = derivative of a P(s) curve.
+    :param clr: cooler.Cooler object to calculate curves from.
+    :param smooth: whether to perform smoothing of the P(s) curve. Default is False.
+    :param aggregate_smoothed: whether to aggregate curves. Default is False.
+    :param nproc: number of processors to use. Default is 4.
+    :returns: a plt.Axes object with plotted P(s) curve and derivative.
+    """
+
+    resolution = clr.binsize
+
+    cvd = cooltools.expected_cis(clr=clr,
+                                 smooth=smooth,
+                                 aggregate_smoothed=aggregate_smoothed,
+                                 nproc=nproc)
+    
+    cvd['s_bp'] = cvd['dist'] * resolution
+
+    if ax is None:
+        _, ax = plt.subplots(**kwargs)
+
+    if smooth and aggregate_smoothed:
+        cvd['balanced.avg.smoothed'].loc[cvd['dist'] < 2] = np.nan
+        cvd = cvd.drop_duplicates(subset=['dist'])
+        cvd = cvd[3:]  # removes peak values at the beginning for prokaryotes
+
+        ys = cvd['balanced.avg.smoothed.agg']
+    
+    if smooth and not aggregate_smoothed:
+        ys = cvd['balanced.avg.smoothed']
+    
+    else:
+        ys = cvd['balanced.avg']
+
+    xs = cvd['s_bp']
+
+    ax.loglog(xs, ys)
+
+    ax.set_ylabel('IC contact frequency')
+
+    scaling_curve.data = cvd
+
+    return ax
+
+
+def scaling_curve_der(
+        clr: cooler.Cooler,
+        c: Optional[str] = None,
+        nproc: Optional[int] = 4,
+        label: Optional[str] = None,
+        ax: Optional[plt.Axes] = None,
+        **kwargs) -> plt.Axes:
+    """
+    Plots a scaling curve derivative in semi-log scale.
+    Data used for plotting can be accessed via attributes of the function:
+    scaling_curve.data = derivative of a P(s) curve.
+    :param clr: cooler.Cooler object to calculate curves from.
+    :param c: color of the line.
+    :param nproc: number of processors to use. Default is 4.
+    :returns: a plt.Axes object with plotted P(s) curve and derivative.
+    """
+
+    resolution = clr.binsize
+
+    cvd = cooltools.expected_cis(clr=clr,
+                                 smooth=True,
+                                 aggregate_smoothed=True,
+                                 nproc=nproc
+                                 )
+    
+    cvd['s_bp'] = cvd['dist'] * resolution
+
+    if ax is None:
+        _, ax = plt.subplots(**kwargs)
+
+    cvd['balanced.avg.smoothed'].loc[cvd['dist'] < 2] = np.nan
+    cvd = cvd.drop_duplicates(subset=['dist'])
+
+
+    cvd['der'] = np.gradient(np.log(cvd['balanced.avg.smoothed.agg']),
+                        np.log(cvd['s_bp'])
+                        )
+    cvd = cvd[3:]  # removes peak values at the beginning for prokaryotes
+
+    xs = cvd['s_bp']
+    ys = cvd['der']
+
+
+    ax.semilogx(xs, ys, color=c, label=label)
+
+    ax.set(xlabel='separation, bp',
+           ylabel='slope'
+           )
+
+    scaling_curve_der.data = cvd
+
+    return ax
+
+
 
 def plot_hic(hic: np.ndarray,
              vmin: float = 0,
